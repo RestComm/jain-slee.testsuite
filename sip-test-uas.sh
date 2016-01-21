03c3b210bf6d0018c9be19f9f3cdf36ab06039fe
SergeyLee <sergey.povarnin@mobius-software.com>
2016-01-21 12:36:46 +0200

    Added scripts for SIP Tests.

---
+++ sip-test-uas.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+### SIP UAS
+
+# Deploy
+echo -e "\nDeploy SIP UAS Example\n"
+cd $HOME/examples/sip-uas
+ant deploy-all
+sleep 10
+
+echo -e "\nTesting SIP UAS Example"
+
+echo -e "\nStart Single test\n"
+cd sipp
+$SIPP 127.0.0.1:5060 -inf users.csv -trace_err -sf uac.xml -i 127.0.0.1 -p 5050 -r 1 -m 10 -l 100 -bg
+
+UAC_PID=$(ps aux | grep '[u]ac.xml' | awk '{print $2}')
+if [ "$UAC_PID" == "" ]; then exit -1; fi
+echo "UAC_PID: $UAC_PID"
+
+#sleep 120s
+TIME=0
+while :; do
+  sleep 1
+  TIME=$((TIME+1))
+  TEST=$(ps aux | grep '[u]ac.xml' | awk '{print $2}')
+  if [ "$TEST" != "$UAC_PID" ]; then break; fi
+done
+
+SIP_UAS_EXIT=$?
+echo -e "SIP UAS Simple Test result: $SIP_UAS_EXIT for $TIME seconds\n" >> $REPORTS/siptests-report.log
+echo -e "\nFinish Single test"
+
+echo -e "\nStart Performance test\n"
+$SIPP 127.0.0.1:5060 -inf users.csv -trace_err -sf uac.xml -i 127.0.0.1 -p 5050 -r 1000 -rp 90s -m 1200 -l 1000 -bg
+#$SIPP 127.0.0.1:5060 -inf users.csv -trace_err -sf uac.xml -i 127.0.0.1 -p 5050 -r 50 -m 1200 -l 1000 -bg
+
+UAC_PID=$(ps aux | grep '[u]ac.xml' | awk '{print $2}')
+if [ "$UAC_PID" == "" ]; then
+  exit -1
+fi
+echo "UAC_PID: $UAC_PID"
+
+#sleep 210s
+TIME=0
+while :; do
+  sleep 1
+  TIME=$((TIME+1))
+  TEST=$(ps aux | grep '[u]ac.xml' | awk '{print $2}')
+  if [ "$TEST" != "$UAC_PID" ]; then
+    break
+  fi
+done
+
+SIP_UAS_PERF_EXIT=$?
+echo -e "SIP UAS Performance Test result: $SIP_UAS_PERF_EXIT for $TIME seconds\n" >> $REPORTS/siptests-report.log
+echo -e "\nFinish Performace test"
+
+# Undeploy
+echo -e "\nUndeploy SIP UAS Example\n"
+cd ..
+ant undeploy-all
+sleep 10
---
+++ sip-test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export HOME=$PWD
+export LOG=$HOME/test-logs
+export REPORTS=$HOME/test-reports
+export SIPP=$HOME/test-tools/sipp/sipp
+
+# Start JSLEE
+export JBOSS_HOME=$HOME/jboss-5.1.0.GA
+echo $JBOSS_HOME
+
+rm -f $LOG/siptests-jboss.log
+rm -f $REPORTS/siptests-report.log
+mkdir -p $LOG
+mkdir -p $REPORTS
+$JBOSS_HOME/bin/run.sh > $LOG/siptests-jboss.log 2>&1 &
+JBOSS_PID="$!"
+echo "JBOSS_PID: $JBOSS_PID"
+
+sleep 30
+
+echo -e "SIP Tests Report\n" >> $REPORTS/siptests-report.log
+
+echo -e "Exit code:
+    0: All calls were successful
+    1: At least one call failed
+   97: exit on internal command. Calls may have been processed
+   99: Normal exit without calls processed
+   -1: Fatal error
+   -2: Fatal error binding a socket\n" >> $REPORTS/siptests-report.log
+
+# SIP UAS
+./sip-test-uas.sh
+
+# SIP B2BUA
+./sip-test-b2bua.sh
+
+# SIP Wake Up
+# SIP JDBC Registrar
+
+pkill -TERM -P $JBOSS_PID
