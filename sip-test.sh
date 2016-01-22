466021dafb7dcec0f92de28bad55b1a3a814f6d0
SergeyLee <sergey.povarnin@mobius-software.com>
2016-01-22 13:49:51 +0200

    Fixed $REPORT.

--- sip-test.sh
+++ sip-test.sh
@@ -3,27 +3,24 @@
 export HOME=$PWD
 export LOG=$HOME/test-logs
 export REPORTS=$HOME/test-reports
+export REPORT=$REPORTS/siptests-report.log
 export SIPP=$HOME/test-tools/sipp/sipp
-#export SIPP=sipp
-
-sipp -v
-$SIPP -v
 
 # Start JSLEE
 export JBOSS_HOME=$HOME/jboss-5.1.0.GA
 echo $JBOSS_HOME
 
 rm -f $LOG/siptests-jboss.log
-rm -f $REPORTS/siptests-report.log
+rm -f $REPORT
 mkdir -p $LOG
 mkdir -p $REPORTS
 $JBOSS_HOME/bin/run.sh > $LOG/siptests-jboss.log 2>&1 &
 JBOSS_PID="$!"
 echo "JBOSS_PID: $JBOSS_PID"
 
-sleep 45
+sleep 30
 
-echo -e "SIP Tests Report\n" >> $REPORTS/siptests-report.log
+echo -e "SIP Tests Report\n" >> $REPORT
 
 echo -e "Exit code:
     0: All calls were successful
@@ -31,7 +28,7 @@ echo -e "Exit code:
    97: exit on internal command. Calls may have been processed
    99: Normal exit without calls processed
    -1: Fatal error
-   -2: Fatal error binding a socket\n" >> $REPORTS/siptests-report.log
+   -2: Fatal error binding a socket\n" >> $REPORT
 
 # SIP UAS
 #./sip-test-uas.sh
@@ -41,5 +38,6 @@ echo -e "Exit code:
 
 # SIP Wake Up
 # SIP JDBC Registrar
+#./sip-test-misc.sh
 
 pkill -TERM -P $JBOSS_PID
