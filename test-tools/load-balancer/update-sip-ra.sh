3160c0005be225ad01642012d0886029e3813d7e
SergeyLee <sergey.povarnin@mobius-software.com>
2016-01-28 18:00:11 +0200

    Fixed.

--- test-tools/load-balancer/update-sip-ra.sh
+++ test-tools/load-balancer/update-sip-ra.sh
@@ -2,7 +2,6 @@
 # $1 - path to Node
 # $2 - new deploy-config.xml
 
-export TEST=$PWD
 export NODE1=$1 #/opt/mobicents/mobicents-slee-2.8.14.40/jboss-5.1.0.GA/server/port-1
 
 export SIP11RAPATH=$NODE1/deploy/sip11-ra-DU-*.jar
@@ -17,7 +16,7 @@ rm -f $SIP11RA.jar
 echo "2"
 cd $SIP11RA/META-INF
 rm deploy-config.xml
-cp $TEST/$2 deploy-config.xml
+cp $2 deploy-config.xml
 
 echo "3"
 cd $NODE1/deploy/$SIP11RA
